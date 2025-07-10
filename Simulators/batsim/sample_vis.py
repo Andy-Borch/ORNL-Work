91c303612ed953547a05bd7eeef882874f3216a4
Anderson Borch (er3) <er3@hs-5600j307-06.ornl.gov>
2025-07-10 14:16:52 -0400

    Added program to plot data from output jobs file

--- Simulators/batsim/sample_vis.py
+++ Simulators/batsim/sample_vis.py
@@ -2,8 +2,26 @@ from evalys.jobset import JobSet
 from evalys import visu
 import matplotlib.pyplot as plt
 
-js = JobSet .from_csv("/home/er3/ORNL-Work/Simulators/batsim/out_jobs.csv")
-js.plot(with_details=True)
+js = JobSet.from_csv("/home/er3/ORNL-Work/Simulators/batsim/out_jobs.csv")
+
+
+visu.gantt.plot_gantt(js)
+
+plt.savefig("./gantt_chart.png", dpi=3000, bbox_inches='tight')
 
 plt.show()
 
+# coding: utf-8
+import matplotlib.pyplot as plt
+from evalys.jobset import JobSet
+
+#matplotlib.use('WX')
+
+js = JobSet.from_csv("/home/er3/ORNL-Work/Simulators/batsim/out_jobs.csv")
+print(js.df.describe())
+
+js.df.hist()
+
+#fig, axe = plt.subplots()
+js.gantt()
+plt.show()
